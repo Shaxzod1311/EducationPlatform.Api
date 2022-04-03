afd6c2efeac19ef012b6ecdee562ea57bfef4fa2
Shaxzod1311 <97043620+Shaxzod1311@users.noreply.github.com>
2022-04-03 22:49:40 +0500

    connection Dbcontext changing

--- EducationPlatform.Api/Startup.cs
+++ EducationPlatform.Api/Startup.cs
@@ -31,7 +31,7 @@ namespace EducationPlatform.Api
         {
 
             services.AddDbContext<EducationDbContext>(options
-                => options.UseNpgsql(Configuration.GetConnectionString("TestDb"))); 
+                => options.UseNpgsql(Configuration.GetConnectionString("EducationDb"))); 
 
             services.AddControllers();
             services.AddSwaggerGen(c =>
@@ -51,7 +51,7 @@ namespace EducationPlatform.Api
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
-            if (env.IsDevelopment())
+            if (env.IsDevelopment() || env.IsProduction())
             {
                 app.UseDeveloperExceptionPage();
                 app.UseSwagger();
