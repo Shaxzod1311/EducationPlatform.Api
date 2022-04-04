931b082ff2e3b897e5381408bd29be6c2aa5d7c1
Shaxzod1311 <97043620+Shaxzod1311@users.noreply.github.com>
2022-04-04 08:25:35 +0500

    add constraint check phone number

--- EducationPlatform.Service/DTOs/CourseCreationViewModel.cs
+++ EducationPlatform.Service/DTOs/CourseCreationViewModel.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.AspNetCore.Http;
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -9,6 +10,7 @@ namespace EducationPlatform.Service.DTOs
 {
     public class CourseCreationViewModel
     {
+        [MinLength(5), MaxLength(30)]
         public string Name { get; set; }
         public string Description { get; set; }
         public short Chit { get; set; }
--- EducationPlatform.Service/DTOs/TeacherCreationViewModel.cs
+++ EducationPlatform.Service/DTOs/TeacherCreationViewModel.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.AspNetCore.Http;
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -11,6 +12,9 @@ namespace EducationPlatform.Service.DTOs
     {
         public string FirstName { get; set; }
         public string LastName { get; set; }
+
+        [Required(ErrorMessage = "This field is required, input valid 12 digit number")]
+        [RegularExpression(@"[0-9][0-9][0-9][0-9][0-9][0-9][0-9][0-9][0-9][0-9][0-9][0-9]")]
         public string Phone { get; set; }
         public IFormFile Image { get; set; }
     }
